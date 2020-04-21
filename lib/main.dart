f15d07472bd390fc304336defb9b364531952676
SviatoslavIlliashenko <Sviatoslav.illiashenko@nure.ua>
2020-04-21 13:37:06 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum AppTheme {
+  GreenLight,
+  GreenDark,
+  BlueLight,
+  BlueDark,
+}
+
+final appThemeData = {
+  AppTheme.GreenLight: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.green,
+  ),
+  AppTheme.GreenDark: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.green[700],
+  ),
+  AppTheme.BlueLight: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+  ),
+  AppTheme.BlueDark: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue[700],
+  ),
+};
+
+@immutable
+abstract class ThemeEvent extends Equatable {
+  // Passing class fields in a list to the Equatable super class
+  ThemeEvent([List props = const []]) : super(props);
+}
+
+class ThemeChanged extends ThemeEvent {
+  final AppTheme theme;
+
+  ThemeChanged({
+    @required this.theme,
+  }) : super([theme]);
+}
+
+
+
+@immutable
+class ThemeState extends Equatable {
+  final ThemeData themeData;
+
+  ThemeState({
+    @required this.themeData,
+  }) : super([themeData]);
+}
+
+
+
+class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
+  @override
+  ThemeState get initialState =>
+      // Everything is accessible from the appThemeData Map.
+  ThemeState(themeData: appThemeData[AppTheme.GreenLight]);
+
+  @override
+  Stream<ThemeState> mapEventToState(
+      ThemeEvent event,
+      ) async* {
+    if (event is ThemeChanged) {
+      yield ThemeState(themeData: appThemeData[event.theme]);
+    }
+  }
+}
+
+
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to the PreferencePage
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => PreferencePage(),
+              ));
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+            'Home',
+            style: Theme.of(context).textTheme.display1,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class PreferencePage extends StatelessWidget {
+  const PreferencePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferences'),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(8),
+        itemCount: AppTheme.values.length,
+        itemBuilder: (context, index) {
+          // Enums expose their values as a list - perfect for ListView
+          // Store the theme for the current ListView item
+          final itemAppTheme = AppTheme.values[index];
+          return Card(
+            // Style the cards with the to-be-selected theme colors
+            color: appThemeData[itemAppTheme].primaryColor,
+            child: ListTile(
+              title: Text(
+                itemAppTheme.toString(),
+                // To show light text with the dark variants...
+                style: appThemeData[itemAppTheme].textTheme.body1,
+              ),
+              onTap: () {
+                // This will make the Bloc output a new ThemeState,
+                // which will rebuild the UI because of the BlocBuilder in main.dart
+                BlocProvider.of<ThemeBloc>(context)
+                    .dispatch(ThemeChanged(theme: itemAppTheme));
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      builder: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme,
+      ),
+    );
+  }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
+    return MaterialApp(
+      title: 'Material App',
+      home: HomePage(),
+      theme: state.themeData,
+    );
+  }
+}
